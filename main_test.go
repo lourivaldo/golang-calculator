45fa2382d39a01b9a43cad23a21595bd4b2c38cd
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-04-17 20:59:04 -0300

    remove compose

--- main_test.go
+++ main_test.go
@@ -4,9 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T){
 
-  t.Log("TestSoma")
-
-  resultado := Soma(5, 6)
+  resultado := Soma(5, 5)
   if resultado != 10 {
     t.Errorf("Soma esperada: %d, obtida: %d", 10, resultado)
   }
